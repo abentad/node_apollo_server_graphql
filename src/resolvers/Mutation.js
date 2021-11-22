ea4c73c6d8e906a9b43e7a99453770ff105089ae
Abenezer <abentad4@gmail.com>
2021-11-22 19:09:39 +0300

    update and delete post subscription done

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -38,23 +38,40 @@ const Mutation = {
         if(!userExists) throw new Error('User doesn\'t exist.');
         const newPost = { id: uuidv4(), ...args.data};
         db.posts.push(newPost);
-        if(newPost.published) pubsub.publish('post', { post: newPost });
+        if(newPost.published) pubsub.publish('post', { post: { mutation: 'CREATED', data: newPost } });
         return newPost;
     },
-    deletePost(parent, args, { db }, info){
+    deletePost(parent, args, { db, pubsub }, info){
         const postIndex = db.posts.findIndex((post)=> post.id === args.id);
         if(postIndex === -1) throw new Error('Post not found');
         db.comments = db.comments.filter((comment)=> comment.postId !== args.id);
-        const removedPost = db.posts.splice(postIndex, 1);
-        return removedPost[0];
+        const [removedPost] = db.posts.splice(postIndex, 1);
+        if(removedPost.published) {
+            pubsub.publish('post', { post: {mutation: 'DELETED', data: removedPost} });
+        }
+        return removedPost;
     },
-    updatePost(parent, args, { db }, info){
+    updatePost(parent, args, { db, pubsub }, info){
         const { id, data } = args;
         const post = db.posts.find((post)=> post.id === id);
+        const originalPost = { ...post };
         if(!post) throw new Error('Post not found');
         if(typeof(data.title) === 'string') post.title = data.title;
         if(typeof(data.body) === 'string') post.body = data.body;
-        if(typeof(data.published) === 'boolean') post.published = data.published;
+        if(typeof(data.published) === 'boolean') {
+            post.published = data.published;
+            if(originalPost.published && !post.published){
+                //DELETED post Subscription
+                pubsub.publish('post', { post: {mutation: 'DELETED', data: originalPost }});
+            }else if(!originalPost.published && post.published){
+                //CREATED post Subscription
+                pubsub.publish('post', { post: {mutation: 'CREATED', data: post }});
+            }
+        }
+        if(originalPost.published){
+            //UPDATED post subscription
+            pubsub.publish('post', { post: {mutation: 'UPDATED', data: post }});
+        }
         return post;
     },
     createComment(parent, args, { db, pubsub }, info){
